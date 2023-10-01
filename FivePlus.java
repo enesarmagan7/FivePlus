c344d4d1604ef8c61f2766e0f37a65ae1df73ce9
enesarmagan7 <144248210+enesarmagan7@users.noreply.github.com>
2023-10-01 20:45:34 +0300

    Add files via upload

---
+++ FivePlus.java
@@ -0,0 +1,23 @@
+package Week2;
+
+public class FivePlus {
+
+    public static void main(String[] args) {
+        int n = 25; // Kullanıcıdan alınan değer
+        recursiveIslem(n);
+    }
+
+    public static void recursiveIslem(int n) {
+        // Base case: n 0 veya negatif olduğunda işlemi sonlandır
+        if (n <= 0) {
+            System.out.println("Son Değer: " + n);
+        } else {
+            // 5 çıkar ve son değeri yazdır
+            System.out.println("Çıkarma Sonucu: " + n);
+            recursiveIslem(n - 5); // Recursive çağrı
+
+            // 5 ekle ve son değeri yazdır
+            System.out.println("Toplama Sonucu: " + n);
+        }
+    }
+}
